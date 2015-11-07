ff6919be4054b766cf743d52761159b82b07f05d
MYE1102 <mye1102@voyager.deanza.edu>
2015-11-06 22:01:07 -0800

    Map and Pacman
    The map and Pacman is working!
    Did work on the collision yet!

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ cookieSprite.setPosition(rand() % 750, rand() % 550);
 
 void handleWallCollision(sf::Sprite *tileArr, sf::Sprite& PacmanSprite)
 {
-if (overlap(tileArr, PacmanSprite))
+if (overlap(*tileArr, PacmanSprite))
 {
-	
+
 
 //eaten++;
 //reset(cookieSprite);
@@ -26,41 +26,45 @@ if (overlap(tileArr, PacmanSprite))
 }
 }
 */
-void handleEvent(sf::RenderWindow& window)
+void handleEvent(sf::RenderWindow& window, sf::Event &event)
 {
-	sf::Event event;
 	while (window.pollEvent(event))
 	{
 		if (event.type == sf::Event::Closed)
 			window.close();
 	}
 }
-/* //Pacman
-void update(sf::Sprite& PacmanSprite, bool arr[][COL])
+
+
+//Pacman
+void update(sf::Sprite& PacmanSprite, sf::Event event)
 {
-if (event.key.code == sf::Keyboard::Left) //keeps it moving even though key is not pressed
-{
-	PacmanSprite.move(0.05, 0);
-}
-else if ((event.key.code == sf::Keyboard::Right))
-{
-	PacmanSprite.move(0.05, 0);
-}
-else if ((event.key.code == sf::Keyboard::Up)
-{
-	PacmanSprite.move(0, -0.05);
-}
-else if ((event.key.code == sf::Keyboard::Down)
-{
-	PacmanSprite.move(0, 0.05);
-}
+	if (event.key.code == sf::Keyboard::Left) //keeps it moving even though key is not pressed
+	{
+		PacmanSprite.move(-1, 0);
+	}
+	else if ((event.key.code == sf::Keyboard::Right))
+	{
+		PacmanSprite.move(1, 0);
+	}
+	else if ((event.key.code == sf::Keyboard::Up))
+	{
+		PacmanSprite.move(0, -1);
+	}
+	else if ((event.key.code == sf::Keyboard::Down))
+	{
+		PacmanSprite.move(0, 1);
+	}
 
 }
-*/
-void draw(sf::RenderWindow& window)
+
+void draw(sf::RenderWindow& window, sf::Sprite pacmanSprite, int counter)
 {
-	window.clear();
-	
+	window.draw(pacmanSprite);
+	//for (int i = 0; i < counter; i++)
+	//{
+	//	window.draw(tileArr[i]);
+	//}
 	window.display();
 }
 
@@ -68,7 +72,7 @@ void draw(sf::RenderWindow& window)
 void drawWall(sf::RenderWindow& window, bool arr[][COL], sf::Sprite *tileArr, sf::Texture wallTexture)
 {
 	int i = 0;
-	//insert wall sprite
+
 	for (int r = 0; r < ROW; r++)
 	{
 		for (int c = 0; c < COL; c++)
@@ -76,17 +80,12 @@ void drawWall(sf::RenderWindow& window, bool arr[][COL], sf::Sprite *tileArr, sf
 			if (arr[r][c] == true)
 			{
 				tileArr[i] = sf::Sprite(wallTexture);
-				tileArr[i].setPosition(r * 64, c * 64); 
+				tileArr[i].setPosition(c * 64, r * 64);
 				window.draw(tileArr[i]);
-				// place the sprite
-				// this need to be the new sprite every time.
-				// use vector
 				i++;
 			}
 		}
 	}
-	window.display();
-
 }
 
 int main()
@@ -97,6 +96,12 @@ int main()
 
 	sf::Texture wallTexture;
 	wallTexture.loadFromFile(resourcePath() + "assets/wall.png");
+
+	sf::Texture pacmanTexture;
+	pacmanTexture.loadFromFile(resourcePath() + "assets/pacman.png");
+	sf::Sprite pacmanSprite(pacmanTexture);
+	pacmanSprite.setOrigin(32, 32);
+	pacmanSprite.setPosition(0 + 32, 4 * 64 + 32);
 
 	sf::Sprite *tileArray = NULL;
 
@@ -109,7 +114,7 @@ int main()
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1 },
 		{ 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
-		{ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
 		{ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1 },
 		{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
@@ -126,7 +131,7 @@ int main()
 			{
 				wall[r][c] = true; // there is a wall
 				counter++;
-			}			
+			}
 			if (map[r][c] == 0)
 				wall[r][c] = false; // blank
 		}
@@ -134,16 +139,17 @@ int main()
 
 	tileArray = new sf::Sprite[counter];
 
-	drawWall(window, wall, tileArray, wallTexture);
-
 	while (window.isOpen())
 	{
-		handleEvent(window);
-
 		window.clear();
-		
+
+		sf::Event event;
+		handleEvent(window, event);
+
+		update(pacmanSprite, event);
+		drawWall(window, wall, tileArray, wallTexture);
+		draw(window, pacmanSprite, counter);
+
 	}
-	
-	//delete tileArray;
 	return 0;
 }
